11677310699d032be32053b5b6d2cca177be283a
tortmac1 <tortmacdelaletra@gmail.com>
2022-07-03 01:49:15 +0200

    implementación de las tareas en fragments y RecyclerView

--- app/src/main/java/com/example/todolist_jose_gilabert_camilo_alzate/MainActivity.java
+++ app/src/main/java/com/example/todolist_jose_gilabert_camilo_alzate/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> list = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    protected void AddToList(String string){
+        list.add(string);
+    }
+
 }
---
+++ app/src/main/java/com/example/todolist_jose_gilabert_camilo_alzate/Tareas.java
@@ -0,0 +1,44 @@
+package com.example.todolist_jose_gilabert_camilo_alzate;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+
+public class Tareas extends Fragment {
+
+    LinearLayoutManager linearLayoutManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tareas, container, false);
+        Button best;
+        best = view.findViewById(R.id.btn_tarea);
+        best.setOnClickListener(view1 -> {
+
+        });
+        return view;
+    }
+
+
+}
