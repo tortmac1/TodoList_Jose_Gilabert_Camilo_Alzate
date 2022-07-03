32851ca5925cabb22e238ad481f3642a189acb80
tortmac1 <tortmacdelaletra@gmail.com>
2022-07-03 23:30:59 +0200

    Funcionamiento de app basica, probemas al intentar agregar complejidad, version de revisión

--- app/src/main/java/com/example/todolist_jose_gilabert_camilo_alzate/MainActivity.java
+++ app/src/main/java/com/example/todolist_jose_gilabert_camilo_alzate/MainActivity.java
@@ -6,17 +6,47 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<String> list = new ArrayList<String>();
+    //ArrayList<String> list = new ArrayList<String>();
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.app_basica);
+        // ADD HERE
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("Sacar a perro");
+        items.add("Comprar el pan");
+        items.add("Revisar el correo de la salle");
+        items.add("Preparar reuniones del día");
+        items.add("Hacer ejercicio");
+
+
+
+
+
+
+        /*
+
         setContentView(R.layout.main);
+
+
 
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = manager.beginTransaction();
@@ -30,10 +60,18 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
+        */
 
     }
 
-    protected void AddToList (String cadena){
-        list.add(cadena);
+    public void addTarea(View v) {
+        EditText nuevaTarea = (EditText) findViewById(R.id.texto_tarea);
+        String itemText = nuevaTarea.getText().toString();
+        itemsAdapter.add(itemText);
+        nuevaTarea.setText("");
+    }
+
+    protected void AddToList(String cadena) {
+       // list.add(cadena);
     }
 }
